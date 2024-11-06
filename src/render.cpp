95aade43e378b7fa2f645356b5be123c84b24119
Paul Chana <paul@elf-audio.com>
2024-11-06 09:50:31 +0000

    Use some global tables

--- src/render.cpp
+++ src/render.cpp
@@ -3,10 +3,7 @@
 #include <stdlib.h>
 
 #include "render.h"
-#include "RenderTabs.h"
-
-
-
+#include "tables.h"
 
 //extern unsigned char A, X, Y;
 //extern unsigned char mem44;
@@ -14,7 +11,6 @@
 extern unsigned char speed;
 extern unsigned char pitch;
 extern int singmode;
-
 
 extern unsigned char phonemeIndexOutput[60]; //tab47296
 extern unsigned char stressOutput[60]; //tab47365
@@ -32,75 +28,56 @@ unsigned char amplitude3[256];
 
 unsigned char sampledConsonantFlag[256]; // tab44800
 
-
 void AddInflection(unsigned char mem48, unsigned char X);
 
 //return = hibyte(mem39212*mem39213) <<  1
-unsigned char trans(unsigned char a, unsigned char b)
-{
-    return (((unsigned int)a * b) >> 8) << 1;
+unsigned char trans(unsigned char a, unsigned char b) {
+	return (((unsigned int) a * b) >> 8) << 1;
 }
-
-
-
 
 // contains the final soundbuffer
 extern int bufferpos;
 extern char *buffer;
 
-
-
 //timetable for more accurate c64 simulation
-static const int timetable[5][5] =
-{
-	{162, 167, 167, 127, 128},
-	{226, 60, 60, 0, 0},
-	{225, 60, 59, 0, 0},
-	{200, 0, 0, 54, 55},
-	{199, 0, 0, 54, 54}
-};
+static const int timetable[5][5] = {
+	{162, 167, 167, 127, 128}, {226, 60, 60, 0, 0}, {225, 60, 59, 0, 0}, {200, 0, 0, 54, 55}, {199, 0, 0, 54, 54}};
 
-void Output(int index, unsigned char A)
-{
+void Output(int index, unsigned char A) {
 	static unsigned oldtimetableindex = 0;
 	int k;
 	bufferpos += timetable[oldtimetableindex][index];
 	oldtimetableindex = index;
 	// write a little bit in advance
-	for(k=0; k<5; k++)
-		buffer[bufferpos/50 + k] = (A & 15)*16;
+	for (k = 0; k < 5; k++)
+		buffer[bufferpos / 50 + k] = (A & 15) * 16;
 }
 
-
-static unsigned char RenderVoicedSample(unsigned short hi, unsigned char off, unsigned char phase1)
-{
+static unsigned char RenderVoicedSample(unsigned short hi, unsigned char off, unsigned char phase1) {
 	do {
-		unsigned char bit = 8;
-		unsigned char sample = sampleTable[hi+off];
+		unsigned char bit	 = 8;
+		unsigned char sample = sam::render_tables::sampleTable[hi + off];
 		do {
 			if ((sample & 128) != 0) Output(3, 26);
 			else Output(4, 6);
 			sample <<= 1;
-		} while(--bit != 0);
+		} while (--bit != 0);
 		off++;
 	} while (++phase1 != 0);
 	return off;
 }
 
-static void RenderUnvoicedSample(unsigned short hi, unsigned char off, unsigned char mem53)
-{
-    do {
-        unsigned char bit = 8;
-        unsigned char sample = sampleTable[hi+off];
-        do {
-            if ((sample & 128) != 0) Output(2, 5);
-            else Output(1, mem53);
-            sample <<= 1;
-        } while (--bit != 0);
-    } while (++off != 0);
+static void RenderUnvoicedSample(unsigned short hi, unsigned char off, unsigned char mem53) {
+	do {
+		unsigned char bit	 = 8;
+		unsigned char sample = sam::render_tables::sampleTable[hi + off];
+		do {
+			if ((sample & 128) != 0) Output(2, 5);
+			else Output(1, mem53);
+			sample <<= 1;
+		} while (--bit != 0);
+	} while (++off != 0);
 }
-
-
 
 // -------------------------------------------------------------------------
 //Code48227
@@ -124,7 +101,7 @@ static void RenderUnvoicedSample(unsigned short hi, unsigned char off, unsigned 
 //   44: J*
 //   45: **    257            276
 //   46: **
-// 
+//
 //   66: P*
 //   67: **    743            767
 //   68: **
@@ -156,15 +133,13 @@ static void RenderUnvoicedSample(unsigned short hi, unsigned char off, unsigned 
 //
 // For voices samples, samples are interleaved between voiced output.
 
-
-void RenderSample(unsigned char *mem66, unsigned char consonantFlag, unsigned char mem49)
-{     
+void RenderSample(unsigned char *mem66, unsigned char consonantFlag, unsigned char mem49) {
 	// mem49 == current phoneme's index
 
-	// mask low three bits and subtract 1 get value to 
+	// mask low three bits and subtract 1 get value to
 	// convert 0 bits on unvoiced samples.
-	unsigned char hibyte = (consonantFlag & 7)-1;
-	
+	unsigned char hibyte = (consonantFlag & 7) - 1;
+
 	// determine which offset to use from table { 0x18, 0x1A, 0x17, 0x17, 0x17 }
 	// T, S, Z                0          0x18
 	// CH, J, SH, ZH          1          0x1A
@@ -172,19 +147,15 @@ void RenderSample(unsigned char *mem66, unsigned char consonantFlag, unsigned ch
 	// /H                     3          0x17
 	// /X                     4          0x17
 
-    unsigned short hi = hibyte*256;
+	unsigned short hi = hibyte * 256;
 	// voiced sample?
 	unsigned char pitchl = consonantFlag & 248;
-	if(pitchl == 0) {
-        // voiced phoneme: Z*, ZH, V*, DH
+	if (pitchl == 0) {
+		// voiced phoneme: Z*, ZH, V*, DH
 		pitchl = pitches[mem49] >> 4;
-        *mem66 = RenderVoicedSample(hi, *mem66, pitchl ^ 255);
-	}
-	else
-		RenderUnvoicedSample(hi, pitchl^255, tab48426[hibyte]);
+		*mem66 = RenderVoicedSample(hi, *mem66, pitchl ^ 255);
+	} else RenderUnvoicedSample(hi, pitchl ^ 255, sam::render_tables::tab48426[hibyte]);
 }
-
-
 
 // CREATE FRAMES
 //
@@ -194,62 +165,57 @@ void RenderSample(unsigned char *mem66, unsigned char consonantFlag, unsigned ch
 //
 // The parameters are copied from the phoneme to the frame verbatim.
 //
-static void CreateFrames()
-{
+static void CreateFrames() {
 	unsigned char X = 0;
-    unsigned int i = 0;
-    while(i < 256) {
-        // get the phoneme at the index
-        unsigned char phoneme = phonemeIndexOutput[i];
+	unsigned int i	= 0;
+	while (i < 256) {
+		// get the phoneme at the index
+		unsigned char phoneme = phonemeIndexOutput[i];
 		unsigned char phase1;
 		unsigned phase2;
-	
-        // if terminal phoneme, exit the loop
-        if (phoneme == 255) break;
-	
-        if (phoneme == PHONEME_PERIOD)   AddInflection(RISING_INFLECTION, X);
-        else if (phoneme == PHONEME_QUESTION) AddInflection(FALLING_INFLECTION, X);
 
-        // get the stress amount (more stress = higher pitch)
-        phase1 = tab47492[stressOutput[i] + 1];
-	
-        // get number of frames to write
-        phase2 = phonemeLengthOutput[i];
-	
-        // copy from the source to the frames list
-        do {
-            frequency1[X] = freq1data[phoneme];     // F1 frequency
-            frequency2[X] = freq2data[phoneme];     // F2 frequency
-            frequency3[X] = freq3data[phoneme];     // F3 frequency
-            amplitude1[X] = ampl1data[phoneme];     // F1 amplitude
-            amplitude2[X] = ampl2data[phoneme];     // F2 amplitude
-            amplitude3[X] = ampl3data[phoneme];     // F3 amplitude
-            sampledConsonantFlag[X] = sampledConsonantFlags[phoneme];        // phoneme data for sampled consonants
-            pitches[X] = pitch + phase1;      // pitch
-            ++X;
-        } while(--phase2 != 0);
-        
-        ++i;
-    }
+		// if terminal phoneme, exit the loop
+		if (phoneme == 255) break;
+
+		if (phoneme == PHONEME_PERIOD) AddInflection(RISING_INFLECTION, X);
+		else if (phoneme == PHONEME_QUESTION) AddInflection(FALLING_INFLECTION, X);
+
+		// get the stress amount (more stress = higher pitch)
+		phase1 = sam::render_tables::tab47492[stressOutput[i] + 1];
+
+		// get number of frames to write
+		phase2 = phonemeLengthOutput[i];
+
+		// copy from the source to the frames list
+		do {
+			frequency1[X] = sam::render_tables::freq1data[phoneme]; // F1 frequency
+			frequency2[X] = sam::render_tables::freq2data[phoneme]; // F2 frequency
+			frequency3[X] = sam::render_tables::freq3data[phoneme]; // F3 frequency
+			amplitude1[X] = sam::render_tables::ampl1data[phoneme]; // F1 amplitude
+			amplitude2[X] = sam::render_tables::ampl2data[phoneme]; // F2 amplitude
+			amplitude3[X] = sam::render_tables::ampl3data[phoneme]; // F3 amplitude
+			sampledConsonantFlag[X] =
+				sam::render_tables::sampledConsonantFlags[phoneme]; // phoneme data for sampled consonants
+			pitches[X] = pitch + phase1; // pitch
+			++X;
+		} while (--phase2 != 0);
+
+		++i;
+	}
 }
-
 
 // RESCALE AMPLITUDE
 //
 // Rescale volume from a linear scale to decibels.
 //
-void RescaleAmplitude() 
-{
-    int i;
-    for(i=255; i>=0; i--)
-        {
-            amplitude1[i] = amplitudeRescale[amplitude1[i]];
-            amplitude2[i] = amplitudeRescale[amplitude2[i]];
-            amplitude3[i] = amplitudeRescale[amplitude3[i]];
-        }
+void RescaleAmplitude() {
+	int i;
+	for (i = 255; i >= 0; i--) {
+		amplitude1[i] = sam::render_tables::amplitudeRescale[amplitude1[i]];
+		amplitude2[i] = sam::render_tables::amplitudeRescale[amplitude2[i]];
+		amplitude3[i] = sam::render_tables::amplitudeRescale[amplitude3[i]];
+	}
 }
-
-
 
 // ASSIGN PITCH CONTOUR
 //
@@ -257,16 +223,14 @@ void RescaleAmplitude()
 // pitch contour. Without this, the output would be at a single
 // pitch level (monotone).
 
-void AssignPitchContour()
-{	
-    int i;
-    for(i=0; i<256; i++) {
-        // subtract half the frequency of the formant 1.
-        // this adds variety to the voice
-        pitches[i] -= (frequency1[i] >> 1);
-    }
+void AssignPitchContour() {
+	int i;
+	for (i = 0; i < 256; i++) {
+		// subtract half the frequency of the formant 1.
+		// this adds variety to the voice
+		pitches[i] -= (frequency1[i] >> 1);
+	}
 }
-
 
 // RENDER THE PHONEMES IN THE LIST
 //
@@ -281,48 +245,47 @@ void AssignPitchContour()
 // 3. Offset the pitches by the fundamental frequency.
 //
 // 4. Render the each frame.
-void Render()
-{
-    unsigned char t;
+void Render() {
+	unsigned char t;
 
 	if (phonemeIndexOutput[0] == 255) return; //exit if no data
 
-    CreateFrames();
-    t = CreateTransitions();
+	CreateFrames();
+	t = CreateTransitions();
 
-    if (!singmode) AssignPitchContour();
-    RescaleAmplitude();
+	if (!singmode) AssignPitchContour();
+	RescaleAmplitude();
 
-    ProcessFrames(t);
+	ProcessFrames(t);
 }
 
-
-// Create a rising or falling inflection 30 frames prior to 
-// index X. A rising inflection is used for questions, and 
+// Create a rising or falling inflection 30 frames prior to
+// index X. A rising inflection is used for questions, and
 // a falling inflection is used for statements.
 
-void AddInflection(unsigned char inflection, unsigned char pos)
-{
-    unsigned char A;
-    // store the location of the punctuation
+void AddInflection(unsigned char inflection, unsigned char pos) {
+	unsigned char A;
+	// store the location of the punctuation
 	unsigned char end = pos;
 
-    if (pos < 30) pos = 0;
-    else pos -= 30;
+	if (pos < 30) pos = 0;
+	else pos -= 30;
 
 	// FIXME: Explain this fix better, it's not obvious
 	// ML : A =, fixes a problem with invalid pitch with '.'
-	while( (A = pitches[pos]) == 127) ++pos;
+	while ((A = pitches[pos]) == 127)
+		++pos;
 
-    while (pos != end) {
-        // add the inflection direction
-        A += inflection;
-	
-        // set the inflection
-        pitches[pos] = A;
+	while (pos != end) {
+		// add the inflection direction
+		A += inflection;
 
-        while ((++pos != end) && pitches[pos] == 255);
-    } 
+		// set the inflection
+		pitches[pos] = A;
+
+		while ((++pos != end) && pitches[pos] == 255)
+			;
+	}
 }
 
 /*
@@ -330,56 +293,51 @@ void AddInflection(unsigned char inflection, unsigned char pos)
     mouth formant (F1) and the throat formant (F2). Only the voiced
     phonemes (5-29 and 48-53) are altered.
 */
-void SetMouthThroat(unsigned char mouth, unsigned char throat)
-{
+void SetMouthThroat(unsigned char mouth, unsigned char throat) {
 	// mouth formants (F1) 5..29
-	static const unsigned char mouthFormants5_29[30] = {
-		0, 0, 0, 0, 0, 10,
-		14, 19, 24, 27, 23, 21, 16, 20, 14, 18, 14, 18, 18,
-		16, 13, 15, 11, 18, 14, 11, 9, 6, 6, 6};
+	static const unsigned char mouthFormants5_29[30] = {0,	0,	0,	0,	0,	10, 14, 19, 24, 27, 23, 21, 16, 20, 14,
+														18, 14, 18, 18, 16, 13, 15, 11, 18, 14, 11, 9,	6,	6,	6};
 
 	// throat formants (F2) 5..29
 	static const unsigned char throatFormants5_29[30] = {
-	255, 255,
-	255, 255, 255, 84, 73, 67, 63, 40, 44, 31, 37, 45, 73, 49,
-	36, 30, 51, 37, 29, 69, 24, 50, 30, 24, 83, 46, 54, 86,
-    };
+		255, 255, 255, 255, 255, 84, 73, 67, 63, 40, 44, 31, 37, 45, 73,
+		49,	 36,  30,  51,	37,	 29, 69, 24, 50, 30, 24, 83, 46, 54, 86,
+	};
 
 	// there must be no zeros in this 2 tables
 	// formant 1 frequencies (mouth) 48..53
 	static const unsigned char mouthFormants48_53[6] = {19, 27, 21, 27, 18, 13};
-       
+
 	// formant 2 frequencies (throat) 48..53
 	static const unsigned char throatFormants48_53[6] = {72, 39, 31, 43, 30, 34};
 
 	unsigned char newFrequency = 0;
-	unsigned char pos = 5;
+	unsigned char pos		   = 5;
 
 	// recalculate formant frequencies 5..29 for the mouth (F1) and throat (F2)
-	while(pos < 30)
-	{
+	while (pos < 30) {
 		// recalculate mouth frequency
 		unsigned char initialFrequency = mouthFormants5_29[pos];
 		if (initialFrequency != 0) newFrequency = trans(mouth, initialFrequency);
-		freq1data[pos] = newFrequency;
-               
+		sam::render_tables::freq1data[pos] = newFrequency;
+
 		// recalculate throat frequency
 		initialFrequency = throatFormants5_29[pos];
-		if(initialFrequency != 0) newFrequency = trans(throat, initialFrequency);
-		freq2data[pos] = newFrequency;
+		if (initialFrequency != 0) newFrequency = trans(throat, initialFrequency);
+		sam::render_tables::freq2data[pos] = newFrequency;
 		pos++;
 	}
 
 	// recalculate formant frequencies 48..53
 	pos = 0;
-    while(pos < 6) {
+	while (pos < 6) {
 		// recalculate F1 (mouth formant)
-		unsigned char initialFrequency = mouthFormants48_53[pos];
-		freq1data[pos+48] = trans(mouth, initialFrequency);
-           
+		unsigned char initialFrequency			= mouthFormants48_53[pos];
+		sam::render_tables::freq1data[pos + 48] = trans(mouth, initialFrequency);
+
 		// recalculate F2 (throat formant)
-		initialFrequency = throatFormants48_53[pos];
-		freq2data[pos+48] = trans(throat, initialFrequency);
+		initialFrequency						= throatFormants48_53[pos];
+		sam::render_tables::freq2data[pos + 48] = trans(throat, initialFrequency);
 		pos++;
 	}
 }
